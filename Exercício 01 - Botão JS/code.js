e7772e06d54062f54dfcbd14e92cd52014ef725f
Thifany Garcia <100360093+projetosthif@users.noreply.github.com>
2023-03-07 11:00:55 -0300

    Add files via upload

---
+++ Exercício 01 - Botão JS/code.js
@@ -0,0 +1,20 @@
+function troca1(){
+    window.location.href = "index.html"
+}
+
+function troca2(){
+
+    window.location.href = "album.html"
+}
+
+function troca3(){
+    window.location.href = "fundamentos.html"
+}
+
+function troca4(){
+    window.location.href = "impactos.html"
+}
+
+function troca5(){
+    window.location.href = "contato.html"
+}
